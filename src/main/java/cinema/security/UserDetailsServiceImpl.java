746aa7067b0e3aa25df2c19bf76eb7feeebd2575
Ilko Good <ilko.engineering@gmail.com>
2023-05-07 11:01:45 +0300

    Initial

---
+++ src/main/java/cinema/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package cinema.security;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import cinema.model.User;
+import cinema.service.UserService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User not found " + email));
+        UserBuilder userBuilder = withUsername(email);
+        userBuilder.password(user.getPassword());
+        userBuilder.authorities(user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return userBuilder.build();
+    }
+}
